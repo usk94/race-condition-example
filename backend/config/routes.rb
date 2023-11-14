735101d167700474bb18a87a689795642b70aa09
Yusaku <hitoyasablue@gmail.com>
2023-11-14 09:31:45 +0900

    update routes

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   namespace 'api' do
     namespace 'v1' do
-      resources :carts
+      resources :carts do
+        collection do
+          post "update"
+        end
+      end
       resources :cards
     end
   end
